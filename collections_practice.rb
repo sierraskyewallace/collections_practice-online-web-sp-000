7ab750593caf2c1c1d72e5b3cc4aca0ac31ed53d
Sierra Wallace <sskwallace@gmail.com>
2020-08-20 23:37:01 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.size <=> b.size}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  array.each {|num| num[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|num| num[0].include?("a")== true}
+end
+
+def sum_array(array)
+  array.inject do |sum, number|
+    sum + number 
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    index != 1 ? element << "s" : element
+  end
+end
